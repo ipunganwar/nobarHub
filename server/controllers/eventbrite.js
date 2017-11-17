8c7c1a7731f91b70c964f7d08b617eeeb0a35254
ipunganwar <ipung.anwar1@gmail.com>
2017-11-17 09:55:20 +0700

    tambah routing

--- server/controllers/eventbrite.js
+++ server/controllers/eventbrite.js
@@ -10,55 +10,67 @@ const events = (req, res) =>{
 	});
 }
 
-const token = (req, res) => {
-	axios.get('https://www.eventbrite.com/oauth/authorize?response_type=code&client_id=PGVEUG6QD2OEHCV3K4RFBIXPAYFJEHAU4QCVVAEPM2OJZNCKS6')
+const about_me = (req, res) => {
+	axios.get('https://www.eventbriteapi.com/v3/users/me/?token=5NZUZAR6DLJ5KUFWKNGT')
 	  .then(function (response) {
 	    res.send(response.data)
 	  })
 	  .catch(function (error) {
-	    console.log(error)
+	    res.send(error)
 	});
 }
 
 
-var _event = {
-    "event.name.html": "Test Event 01",
-    "event.description.html": "Test Event 01",
-    "event.start.timezone": "America/Chicago",
-    "event.start.utc": "2017-07-10T18:00:00Z",
-    "event.end.timezone": "America/Chicago",
-    "event.end.utc": "2017-07-10T20:00:00Z",
-    "event.currency": "USD",
-};
-let  EVENTBRITE_API_URL = 'https://www.eventbriteapi.com/v3/',
+const create = (req, res) => {
+	let  EVENTBRITE_API_URL = 'https://www.eventbriteapi.com/v3/',
 		_token = '5NZUZAR6DLJ5KUFWKNGT'
 
-var _headers = {
+	var _headers = {
     'Authorization': 'Bearer ' + _token,
     'Content-Type': 'application/x-www-form-urlencoded',
-}
+	}
 
-var options = {
-    url: EVENTBRITE_API_URL + "events/?token="+_token,
-    method: 'POST',
-    headers: _headers,
-    form: _event,
-};
+	var _event = {
+	    "event.name.html": req.body["event.name.html"],
+	    "event.description.html": req.body["event.description.html"],
+	    "event.start.timezone": req.body["event.start.timezone"],
+	    "event.start.utc": req.body["event.start.utc"],
+	    "event.end.timezone": req.body["event.end.timezone"],
+	    "event.end.utc": req.body["event.end.utc"],
+	    "event.currency": req.body["event.currency"],
+	};
 
+	var options = {
+	    url: EVENTBRITE_API_URL + "events/?token="+_token,
+	    method: 'POST',
+	    headers: _headers,
+	    form: _event,
+	};
 
-const create = (req, res) => {
 	axios.post(options.url, options.form)
 	  .then(function (response) {
 	    res.send(response.data)
 	  })
 	  .catch(function (error) {
-	    console.log(error)
+	    res.send(error)
 	});
 }
 
+const event_me = (req, res) =>{
+	axios.get('https://www.eventbriteapi.com/v3/users/me/owned_events/?token=5NZUZAR6DLJ5KUFWKNGT')
+	  .then(function (response) {
+	    res.send(response.data)
+	  })
+	  .catch(function (error) {
+	    res.send(error)
+	});
+
+} 
+
 module.exports = {
 	events,
-	token,
-	create
+	about_me,
+	create,
+	event_me
 }
      
--- server/routes/eventbrite.js
+++ server/routes/eventbrite.js
@@ -2,7 +2,8 @@ const router = require('express').Router()
 const eventbriteController = require('../controllers/eventbrite')
 
 router.get('/', eventbriteController.events)
-router.get('/token', eventbriteController.token)
-router.get('/create', eventbriteController.create)
+router.get('/aboutme', eventbriteController.about_me)
+router.post('/create', eventbriteController.create)
+router.get('/me', eventbriteController.event_me)
 
 module.exports = router
